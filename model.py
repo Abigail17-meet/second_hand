dcbec233cbe8debf17a44c97fab38c89341b94e4
george17-meet <george17@meet.mit.edu>
2017-02-02 19:44:07 +0200

    first commit

---
+++ model.py
@@ -0,0 +1,55 @@
+from sqlalchemy import Column,Integer,String, DateTime, ForeignKey, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine, func
+from passlib.apps import custom_app_context as pwd_context
+from datetime import datetime
+
+Base = declarative_base()
+
+
+
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    address = Column(String(255))
+    email = Column(String(255), unique=True)
+    password_hash = Column(String(255))
+    items = relationship("Item", back_populates="customer")
+    comments = relationship("Comment", back_populates="customer")
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    description = Column(String(255))
+    picture_link = Column(String(255))
+    price = Column(String(255))
+    location = Column(String(255))
+    customer = relationship("Customer", back_populates="items")
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    comments = relationship("Comment", back_populates="item")
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    #now = datetime.now()
+    id = Column(Integer, primary_key=True)
+    text = Column(String(255))
+    customer = relationship("Customer", back_populates="comments")
+    item = relationship("Item", back_populates="comments")
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    item_id = Column(Integer, ForeignKey('item.id'))
+
+
+
+engine = create_engine('sqlite:///Second_hand.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine, autoflush=False)
+session = DBSession()
---
+++ populateDB.py
@@ -0,0 +1,63 @@
+from model import *
+
+
+#Make new Users
+
+lorenzo = Customer(name="Lorenzo", email = "lobrown@meet.mit.edu", address = "Moshe Hess 7")
+lorenzo.hash_password("myPassword")
+session.add(lorenzo)
+session.commit()
+
+
+Abigail = Customer(name="Abigail", email = "gugigil@gmail.com", address = "Hashkedim 10")
+Abigail.hash_password("myPassword")
+session.add(Abigail)
+session.commit()
+
+Uria = Customer(name="Uria", email = "uria@gmail.com", address = "Galil 19")
+Uria.hash_password("myPassword")
+session.add(Uria)
+session.commit()
+
+Jana = Customer(name="Jana", email = "jana@meet.mit.edu", address = "derech hayam")
+Jana.hash_password("myPassword")
+session.add(Jana)
+session.commit()
+
+#Make Items
+
+item1 = Item(description = "putsomething here", price = "99", picture_link = "", location = "Tivon")
+item1.customer_id = 1
+session.add(item1)
+session.commit()
+
+item2 = Item(description = "apple", price = "9", picture_link = "", location = "Nazareth")
+item2.customer_id = 4
+session.add(item2)
+session.commit()
+
+item3 = Item(description = "apple", price = "12", picture_link = "", location = "Tel-Aviv")
+item3.customer_id = 3
+session.add(item3)
+session.commit()
+
+
+
+#Make Comments
+comment1 = Comment(text="yummy") 
+comment1.customer_id=1
+comment1.item_id=1
+session.add(comment1)
+session.commit()
+
+comment2 = Comment(text="ewww") 
+comment2.customer_id=4
+comment2.item_id=2
+session.add(comment2)
+session.commit()
+
+comment3 = Comment(text="I would never buy it!") 
+comment3.customer_id=4
+comment3.item_id=3
+session.add(comment3)
+session.commit()
